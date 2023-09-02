fb16a0c3b295104639b205860a4ddffb69f949e9
Christian Berendt <berendt@osism.tech>
2023-09-02 18:14:50 +0200

    Remove unused sbom generator scripts (#53)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/sbom_generator/main.py
+++
@@ -1,9 +0,0 @@
-import get_pypi_requirements
-
-# import repo_manager
-
-# repo_manager.clone_repos(user="osism")
-
-pypi_requirements = get_pypi_requirements.get_requirements()
-
-# repo_manager.delete_repos()
